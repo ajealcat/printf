5958c7de164f6ea3b342a1f2b15f64c9f0d71c62
Anna JEARUTH <ajearuth@student.42.fr>
2021-08-17 11:59:44 +0200

    potites fonctions en plus, on test

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,20 +6,45 @@
 /*   By: ajearuth <ajearuth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 14:42:58 by ajearuth          #+#    #+#             */
-/*   Updated: 2021/08/13 12:39:08 by ajearuth         ###   ########.fr       */
+/*   Updated: 2021/08/17 11:58:16 by ajearuth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "printf.h"
 
-int	ft_printf(const *char, ...)
+static int	ft_is_charset(char *str)
 {
 	int i;
-	va_list stock;
 
 	i = 0;
-	while ()
+	while (str[i])
 	{
-		ft_checkversion(
+		if (str[i] == 'c' || str[i] == 's'|| str[i] == 'p' || str[i] == 'd' ||
+				str[i] == 'i' || str[i] == 'u' || str[i] == 'x' || str[i] == '%')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	ft_printf(const char *str, ...)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] != ''%')
+		{	
+			ft_putchar_fd(str[i], 1);
+			i++;
+		}
+		if (str[i] == '%')
+		{
+			*str = *str + i + 1;
+			while (ft_is_charset(str) == 1)
+				i++;
+		}
 	}
 }
 
