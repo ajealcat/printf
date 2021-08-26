120adb4f6537c8c41044975c8bbc82f8026f1aac
Anna JEARUTH <ajearuth@student.42.fr>
2021-08-26 11:09:49 +0200

    not a bonus hunter

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ajearuth <ajearuth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 14:42:58 by ajearuth          #+#    #+#             */
-/*   Updated: 2021/08/26 10:31:14 by ajearuth         ###   ########.fr       */
+/*   Updated: 2021/08/26 11:04:50 by ajearuth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,20 @@
 
 int	ft_printf(const char *str, ...)
 {
+	int		i;
 	va_list	arg;
 
+	va_start(arg, str);
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '%')
+		{
+			++i;
+			ft_parse(str[i]);
+		}
+		else
+			write(1, &str[i], 1);
+		++i;
+	}
 }
